ae712f31a8039d95614638efc7f8731ec1d0cdf6
Olivier Blanc <olivierhumanbooster@gmail.com>
2022-07-13 09:51:34 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<ul>
+    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis deserunt dicta dolorum ea ex fugit hic, ipsam
+        iste quidem tempore? A at incidunt inventore iusto magni repellat sed, similique temporibus.
+    </li>
+    <li>A ab aperiam, asperiores consequatur et fuga illo ipsam iste iure laborum natus nisi nobis officia omnis
+        perspiciatis praesentium quasi quis ratione reiciendis reprehenderit sequi similique totam unde, vel veritatis?
+    </li>
+    <li>Amet asperiores at beatae cupiditate debitis delectus dicta dignissimos dolores earum est id illum itaque
+        laboriosam laborum minima natus necessitatibus, nulla numquam obcaecati perferendis quam quibusdam quis sint,
+        velit voluptatum!
+    </li>
+    <li>Architecto, culpa ea illo minima modi possimus quia reiciendis repellat, repellendus similique temporibus ut
+        veritatis voluptas! A ad dicta, doloremque eius, illo laborum modi optio provident quae quas sapiente tenetur.
+    </li>
+    <li>At, blanditiis, dicta doloremque enim esse, excepturi fuga itaque iusto mollitia nihil officiis quas qui quia
+        reprehenderit sint temporibus tenetur ullam. Ad blanditiis ducimus odit rerum tempora. Eligendi, impedit odio.
+    </li>
+</ul>
+
+</body>
+</html>
+
